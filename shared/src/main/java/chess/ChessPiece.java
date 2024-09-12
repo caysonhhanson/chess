01a44f29b20062851c4a68f21701bc2e20c9d130
Cayson Hanson <chanballer27@gmail.com>
2024-09-12 15:00:53 -0600

    Initialzing some of the work on chess piece.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -12,8 +12,15 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
-    }
+  private final ChessGame.TeamColor pieceColor;
+  private final PieceType type;
+  private ChessBoard board;
+  private ChessPosition myPosition;
+
+  public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    this.pieceColor=pieceColor;
+    this.type=type;
+  }
 
     /**
      * The various different chess piece options
@@ -31,14 +38,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -49,6 +56,8 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+      this.board=board;
+      this.myPosition=myPosition;
+      return new ArrayList<>();
     }
 }
