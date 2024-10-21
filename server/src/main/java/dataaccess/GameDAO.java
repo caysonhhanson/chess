074aed7d957c8620e7fe022b7df48e7abf0f4788
Cayson Hanson <chanballer27@gmail.com>
2024-10-21 13:54:22 -0600

    Game DAO filled

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,12 @@
 package dataaccess;
 
+import model.GameData;
+import java.util.List;
+
 public interface GameDAO {
+  void insertGame(GameData game) throws DataAccessException;
+  GameData getGame(int gameID) throws DataAccessException;
+  List<GameData> getAllGames() throws DataAccessException;
+  void deleteGame(int gameID) throws DataAccessException;
 }
+
