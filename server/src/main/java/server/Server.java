97ef7a22216d8fbe569ade0a73715226ab59ba32
Cayson Hanson <chanballer27@gmail.com>
2024-11-11 17:40:52 -0700

    Getting Started

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,4 +1,5 @@
 package server;
+
 import dataaccess.*;
 import service.UserService;
 import service.GameService;
@@ -7,35 +8,27 @@ import spark.Spark;
 public class Server {
     private final UserService userService;
     private final GameService gameService;
+
     public Server() {
-        try {
-            DatabaseInitializer.initialize();
-            var userDAO = new SQLUserDAO();
-            var authDAO = new SQLAuthDAO();
-            var gameDAO = new SQLGameDAO();
-            userService = new UserService(userDAO, authDAO);
-            gameService = new GameService(userDAO, gameDAO, authDAO);
-        } catch (DataAccessException e) {
-            System.err.println("Failed to initialize server: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
+        var userDAO = new SQLUserDAO();
+        var authDAO = new SQLAuthDAO();
+        var gameDAO = new SQLGameDAO();
+
+        userService = new UserService(userDAO, authDAO);
+        gameService = new GameService(userDAO, gameDAO, authDAO);
     }
+
     public int run(int desiredPort) {
+        Spark.stop();
+        Spark.awaitStop();
+
         Spark.port(desiredPort);
         Spark.staticFiles.location("web");
-        Spark.options("/*", (request, response) -> {
-            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-            }
-            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-            }
-            return "OK";
-        });
+
+
         var userHandler = new UserHandler(userService);
         var gameHandler = new GameHandler(gameService);
+
         Spark.delete("/db", gameHandler::handleClear);
         Spark.post("/user", userHandler::handleRegister);
         Spark.post("/session", userHandler::handleLogin);
@@ -43,12 +36,15 @@ public class Server {
         Spark.get("/game", gameHandler::handleListGames);
         Spark.post("/game", gameHandler::handleCreateGame);
         Spark.put("/game", gameHandler::handleJoinGame);
+
         Spark.awaitInitialization();
         return Spark.port();
     }
+
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
     }
 }
+
 
