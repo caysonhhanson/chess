fb528f51ea371112d9c9a70dbed37578cc788b4e
Cayson Hanson <chanballer27@gmail.com>
2024-11-13 09:30:42 -0700

    Added the impors and original things needed for my serverfacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,22 @@
 package client;
 
+import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+
 public class ServerFacade {
+  private final String serverUrl;
+  private final Gson gson;
+  private String authToken;
+
+  public ServerFacade(String serverUrl) {
+    this.serverUrl = serverUrl;
+    this.gson = new Gson();
+  }
 }
