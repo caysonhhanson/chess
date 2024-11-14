262e61becf69d9e523fd3e08b11d2ce587710479
Cayson Hanson <chanballer27@gmail.com>
2024-11-14 12:27:33 -0700

    fix serverfacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -9,12 +9,8 @@ import java.util.HashSet;
 import java.util.Map;
 
 public class ServerFacade {
-  String baseURL;
-  String authToken;
-
-  public ServerFacade() {
-    this.baseURL = "http://localhost:8080";
-  }
+  private final String baseURL;
+  private String authToken;
 
   public ServerFacade(int port) {
     this.baseURL = "http://localhost:" + port;
@@ -28,26 +24,23 @@ public class ServerFacade {
       http.setDoOutput(true);
       http.addRequestProperty("Content-Type", "application/json");
 
-      var requestBody = Map.of("username", username, "password", password, "email", email);
-      try (OutputStream os = http.getOutputStream()) {
-        String jsonBody = new Gson().toJson(requestBody);
-        os.write(jsonBody.getBytes());
-      }
+      var requestBody = Map.of(
+              "username", username,
+              "password", password,
+              "email", email
+      );
 
+      writeBody(http, requestBody);
       http.connect();
 
       if (http.getResponseCode() == 200) {
-        try (InputStream respBody = http.getInputStream()) {
-          InputStreamReader reader = new InputStreamReader(respBody);
-          Map<String, String> map = new Gson().fromJson(reader, Map.class);
-          authToken = map.get("authToken");
-          return true;
-        }
+        authToken = readToken(http);
+        return true;
       }
+      return false;
     } catch (Exception ex) {
       return false;
     }
-    return false;
   }
 
   public boolean login(String username, String password) {
@@ -58,36 +51,32 @@ public class ServerFacade {
       http.setDoOutput(true);
       http.addRequestProperty("Content-Type", "application/json");
 
-      var requestBody = Map.of("username", username, "password", password);
-      try (OutputStream os = http.getOutputStream()) {
-        String jsonBody = new Gson().toJson(requestBody);
-        os.write(jsonBody.getBytes());
-      }
+      var requestBody = Map.of(
+              "username", username,
+              "password", password
+      );
 
+      writeBody(http, requestBody);
       http.connect();
 
       if (http.getResponseCode() == 200) {
-        try (InputStream respBody = http.getInputStream()) {
-          InputStreamReader reader = new InputStreamReader(respBody);
-          Map<String, String> map = new Gson().fromJson(reader, Map.class);
-          authToken = map.get("authToken");
-          return true;
-        }
+        authToken = readToken(http);
+        return true;
       }
+      return false;
     } catch (Exception ex) {
       return false;
     }
-    return false;
   }
 
   public boolean logout() {
+    if (authToken == null) return false;
+
     try {
       URI uri = new URI(baseURL + "/session");
       HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
       http.setRequestMethod("DELETE");
-      if (authToken != null) {
-        http.addRequestProperty("Authorization", authToken);
-      }
+      http.addRequestProperty("Authorization", authToken);
 
       http.connect();
 
@@ -95,103 +84,114 @@ public class ServerFacade {
         authToken = null;
         return true;
       }
+      return false;
     } catch (Exception ex) {
       return false;
     }
-    return false;
   }
 
   public int createGame(String gameName) {
+    if (authToken == null) return -1;
+
     try {
       URI uri = new URI(baseURL + "/game");
       HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
       http.setRequestMethod("POST");
       http.setDoOutput(true);
-      if (authToken != null) {
-        http.addRequestProperty("Authorization", authToken);
-      }
+      http.addRequestProperty("Authorization", authToken);
       http.addRequestProperty("Content-Type", "application/json");
 
       var requestBody = Map.of("gameName", gameName);
-      try (OutputStream os = http.getOutputStream()) {
-        String jsonBody = new Gson().toJson(requestBody);
-        os.write(jsonBody.getBytes());
-      }
+      writeBody(http, requestBody);
 
       http.connect();
 
       if (http.getResponseCode() == 200) {
         try (InputStream respBody = http.getInputStream()) {
-          InputStreamReader reader = new InputStreamReader(respBody);
-          Map<String, Double> map = new Gson().fromJson(reader, Map.class);
+          Map<String, Double> map = new Gson().fromJson(new InputStreamReader(respBody), Map.class);
           return map.get("gameID").intValue();
         }
       }
+      return -1;
     } catch (Exception ex) {
       return -1;
     }
-    return -1;
   }
 
   public HashSet<GameData> listGames() {
+    if (authToken == null) return new HashSet<>();
+
     try {
       URI uri = new URI(baseURL + "/game");
       HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
       http.setRequestMethod("GET");
-      if (authToken != null) {
-        http.addRequestProperty("Authorization", authToken);
-      }
+      http.addRequestProperty("Authorization", authToken);
 
       http.connect();
 
       if (http.getResponseCode() == 200) {
         try (InputStream respBody = http.getInputStream()) {
-          InputStreamReader reader = new InputStreamReader(respBody);
-          var response = new Gson().fromJson(reader, Map.class);
+          var response = new Gson().fromJson(new InputStreamReader(respBody), Map.class);
           var games = new HashSet<GameData>();
           var gamesList = (java.util.ArrayList<?>) response.get("games");
+
           for (Object gameObj : gamesList) {
-            var gameMap = (Map<?, ?>) gameObj;
-            var game = new GameData(
+            Map<?, ?> gameMap = (Map<?, ?>) gameObj;
+            games.add(new GameData(
                     ((Double) gameMap.get("gameID")).intValue(),
                     (String) gameMap.get("whiteUsername"),
                     (String) gameMap.get("blackUsername"),
                     (String) gameMap.get("gameName"),
                     new chess.ChessGame()
-            );
-            games.add(game);
+            ));
           }
           return games;
         }
       }
+      return new HashSet<>();
     } catch (Exception ex) {
       return new HashSet<>();
     }
-    return new HashSet<>();
   }
 
   public boolean joinGame(int gameID, String playerColor) {
+    if (authToken == null) return false;
+
     try {
       URI uri = new URI(baseURL + "/game");
       HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
       http.setRequestMethod("PUT");
       http.setDoOutput(true);
-      if (authToken != null) {
-        http.addRequestProperty("Authorization", authToken);
-      }
+      http.addRequestProperty("Authorization", authToken);
       http.addRequestProperty("Content-Type", "application/json");
 
-      var requestBody = Map.of("gameID", gameID, "playerColor", playerColor);
-      try (OutputStream os = http.getOutputStream()) {
-        String jsonBody = new Gson().toJson(requestBody);
-        os.write(jsonBody.getBytes());
-      }
+      var requestBody = Map.of(
+              "gameID", gameID,
+              "playerColor", playerColor
+      );
 
+      writeBody(http, requestBody);
       http.connect();
-      return http.getResponseCode() == 200;
 
+      return http.getResponseCode() == 200;
     } catch (Exception ex) {
       return false;
+    }
+  }
+
+  private void writeBody(HttpURLConnection http, Object request) throws IOException {
+    if (request != null) {
+      http.addRequestProperty("Content-Type", "application/json");
+      String jsonData = new Gson().toJson(request);
+      try (var outputStream = http.getOutputStream()) {
+        outputStream.write(jsonData.getBytes());
+      }
+    }
+  }
+
+  private String readToken(HttpURLConnection http) throws IOException {
+    try (InputStream respBody = http.getInputStream()) {
+      return new Gson().fromJson(new InputStreamReader(respBody), Map.class).get("authToken").toString();
     }
   }
 }
