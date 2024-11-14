6150eb1bfec5a0c6d3cb985387724eab635c36e9
Cayson Hanson <chanballer27@gmail.com>
2024-11-14 12:43:42 -0700

    changed te server main to have dynamic port testing

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,10 +1,22 @@
 import chess.*;
 import server.Server;
+
 public class Main {
+    private static final int DEFAULT_PORT = 8080;
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        System.out.println("240 Chess Server: " + piece);
         Server server = new Server();
-        server.run(8080);
+
+        int port = DEFAULT_PORT;
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port number. Using default port " + DEFAULT_PORT);
+            }
+        }
+        server.run(port);
     }
 }
