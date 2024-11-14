f93f2cc2ea590e993478f46dba74880023bdaf78
Cayson Hanson <chanballer27@gmail.com>
2024-11-14 12:32:28 -0700

    Fixed Main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,15 +1,24 @@
-import chess.*;
 import client.ServerFacade;
 import ui.PreLoginREPL;
 
 public class Main {
+  private static final int DEFAULT_PORT = 8080;
+
   public static void main(String[] args) throws Exception {
     System.out.println("240 Chess Client:");
-    int port = 0;
+
+    int port = DEFAULT_PORT;
+    if (args.length > 0) {
+      try {
+        port = Integer.parseInt(args[0]);
+      } catch (NumberFormatException e) {
+        System.out.println("Invalid port number. Using default port " + DEFAULT_PORT);
+      }
+    }
+
     ServerFacade server = new ServerFacade(port);
     PreLoginREPL prelogin = new PreLoginREPL(server);
     prelogin.run();
     System.out.println("Ended");
-
   }
 }
