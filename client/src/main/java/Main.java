b6f292edfe38341a5850a6e171e4077a58636493
Cayson Hanson <chanballer27@gmail.com>
2024-11-14 12:14:18 -0700

    Main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,2 +1,15 @@
+import chess.*;
+import client.ServerFacade;
+import ui.PreLoginREPL;
+
 public class Main {
+  public static void main(String[] args) throws Exception {
+    System.out.println("240 Chess Client:");
+    int port = 0;
+    ServerFacade server = new ServerFacade(port);
+    PreLoginREPL prelogin = new PreLoginREPL(server);
+    prelogin.run();
+    System.out.println("Ended");
+
+  }
 }
