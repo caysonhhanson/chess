ee9c76a3f24cea29831714123bc03df8b46babab
Cayson Hanson <chanballer27@gmail.com>
2024-11-18 14:44:25 -0700

    TA fixes

--- client/src/main/java/ui/ChessBoardMaker.java
+++ client/src/main/java/ui/ChessBoardMaker.java
@@ -27,27 +27,23 @@ public class ChessBoardMaker {
 
   private static void drawBoardBody(ChessBoard board, boolean blackPerspective) {
     for (int row = 8; row >= 1; row--) {
-      int displayRow = blackPerspective ? 9 - row : row;
+      int displayRow = row;
       System.out.print(SET_TEXT_COLOR_YELLOW + SET_TEXT_BOLD + " " + displayRow + "  " + RESET_TEXT_BOLD_FAINT);
 
       for (int col = 1; col <= 8; col++) {
         int displayCol = blackPerspective ? 9 - col : col;
-        boolean isLightSquare = (displayRow + displayCol) % 2 == 0;
+        boolean isLightSquare = (displayRow + displayCol) % 2 == 1;
 
         String bgColor = isLightSquare ? SET_BG_COLOR_WHITE : SET_BG_COLOR_DARK_GREY;
+        System.out.print(bgColor);
 
-        ChessPosition position = new ChessPosition(row, col);
+        ChessPosition position = new ChessPosition(row, blackPerspective ? 9 - col : col);
         ChessPiece piece = board.getPiece(position);
 
-        String textColor;
-        if (piece != null) {
-          textColor = piece.getTeamColor() == ChessGame.TeamColor.WHITE ?
-                  SET_TEXT_COLOR_BLUE : SET_TEXT_COLOR_RED;
-        } else {
-          textColor = SET_TEXT_COLOR_BLACK;
-        }
+        String textColor = piece != null && piece.getTeamColor() == ChessGame.TeamColor.WHITE ?
+                SET_TEXT_COLOR_BLUE : SET_TEXT_COLOR_RED;
+        System.out.print(textColor);
 
-        System.out.print(bgColor + textColor);
         System.out.print(getPieceString(piece));
         System.out.print(RESET_BG_COLOR);
       }
@@ -56,9 +52,7 @@ public class ChessBoardMaker {
   }
 
   private static String getPieceString(ChessPiece piece) {
-    if (piece == null) {
-      return EMPTY;
-    }
+    if (piece == null) return EMPTY;
 
     return switch (piece.getPieceType()) {
       case KING -> piece.getTeamColor() == ChessGame.TeamColor.WHITE ? WHITE_KING : BLACK_KING;
